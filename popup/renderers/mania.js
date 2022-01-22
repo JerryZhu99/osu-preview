0ddc5defc6bbb26b2852ca9ac89a510fea3fd965
Jerry Zhu <jerryzhu99@gmail.com>
2022-01-21 16:27:15 -0800

    Implement SV calculation

--- popup/renderers/mania.js
+++ popup/renderers/mania.js
@@ -5,8 +5,8 @@ const NOTE_HEIGHT = 15;
 
 const JUDGE_LINE_HEIGHT = 5;
 
-const NOTE_SPEED = 0.75;
-const HIT_POSITION = 384 - 50;
+const NOTE_SPEED = 0.65;
+const HIT_POSITION = 384 - 20;
 
 const X_CENTER = 512 / 2;
 
@@ -53,13 +53,13 @@ const getMainBPM = (timingPoints, hitObjects) => {
         if (i + 1 >= arr.length) {
           endTime = hitObjects.pop().time;
         } else {
-          endTime = arr[i + 1].offset;
+          endTime = arr[i + 1].time;
         }
-        let duration = endTime - point.offset;
-        if (!bpms.has(point.msPerBeat)) {
-          bpms.set(point.msPerBeat, 0);
+        let duration = endTime - point.time;
+        if (!bpms.has(point.ms_per_beat)) {
+          bpms.set(point.ms_per_beat, 0);
         }
-        bpms.set(point.msPerBeat, bpms.get(point.msPerBeat) + duration);
+        bpms.set(point.ms_per_beat, bpms.get(point.ms_per_beat) + duration);
       });
     let mainBpm = 60000 / [...bpms.entries()]
       .reduce(([mainMsPerBeat, maxCount], [msPerBeat, count]) => {
@@ -68,18 +68,49 @@ const getMainBPM = (timingPoints, hitObjects) => {
     return mainBpm;
 }
 
+const computeSVTimeOffset = (time, objectTime, mainBPM, timingPoints) => {
+  let totalOffset = 0;
+  let lastTime = 0;
+  let currentBPM = 60000 / timingPoints[0].ms_per_beat;
+  let currentSV = 1;
+  for (let point of timingPoints) {
+    if (point.time > objectTime) break;
+    if (point.time > time) {
+      const bpmFactor = currentBPM / mainBPM;
+      totalOffset += bpmFactor * currentSV * (point.time - Math.max(lastTime, time));
+    }
+
+    if (point.ms_per_beat > 0) {
+      currentBPM =  60000 / point.ms_per_beat;
+    } else {
+      currentSV = -100 / point.ms_per_beat;
+    }
+
+    lastTime = point.time;
+  }
+
+  if (lastTime < objectTime) {
+    const bpmFactor = currentBPM / mainBPM;
+    totalOffset += bpmFactor * currentSV * (objectTime - Math.max(lastTime, time));
+  }
+
+  return totalOffset;
+}
+
 
 const getLane = (object, keyCount) => {
     const [x] = object.data.pos;
     return Math.floor(Number(x) * keyCount / 512);
 }
 
-const drawHoldNote = (ctx, object, keyCount, time) => {
+const drawHoldNote = (ctx, object, keyCount, time, mainBPM, timingPoints) => {
     const lane = getLane(object, keyCount);
-    
+    const startOffset = computeSVTimeOffset(time, object.time, mainBPM, timingPoints);
+    const holdOffset = computeSVTimeOffset(Math.max(time, object.time), object.endTime, mainBPM, timingPoints);
+
     const xOffset = (lane - keyCount / 2) * LANE_WIDTH + X_CENTER;
-    const yOffset = HIT_POSITION - NOTE_SPEED * Math.max(0, (object.time - time)); 
-    const height = NOTE_SPEED * (object.endTime - Math.max(time, object.time));
+    const yOffset = HIT_POSITION - NOTE_SPEED * Math.max(0, startOffset); 
+    const height = NOTE_SPEED * holdOffset;
 
     if (yOffset < -NOTE_HEIGHT - 64) return;
 
@@ -91,12 +122,12 @@ const drawHoldNote = (ctx, object, keyCount, time) => {
 
 }
 
-const drawNote = (ctx, object, keyCount, time) => {
-    
+const drawNote = (ctx, object, keyCount, time, mainBPM, timingPoints) => {
     const lane = getLane(object, keyCount);
+    const totalOffset = computeSVTimeOffset(time, object.time, mainBPM, timingPoints);
     
     const xOffset = (lane - keyCount / 2) * LANE_WIDTH + X_CENTER;
-    const yOffset = HIT_POSITION - NOTE_SPEED * (object.time - time); 
+    const yOffset = HIT_POSITION - NOTE_SPEED * totalOffset; 
 
     if (yOffset < -NOTE_HEIGHT - 64) return;
 
@@ -117,15 +148,15 @@ export default class ManiaRenderer {
     }
 
     render(time) {
-        const { ctx, keyCount } = this;
+        const { ctx, keyCount, mainBPM, timingPoints } = this;
 
         this.hitObjects
             .filter((object) => object.endTime > time)
             .forEach((object) => {
                 if (isHold(object)) {
-                    drawHoldNote(ctx, object, keyCount, time);
+                    drawHoldNote(ctx, object, keyCount, time, mainBPM, timingPoints);
                 } else {
-                    drawNote(ctx, object, keyCount, time);
+                    drawNote(ctx, object, keyCount, time, mainBPM, timingPoints);
                 }
             });
 
